7f379200b3b008e814688838cdc13edaa454f827
Mojalefa MJAY Moja <87690422+MojalefaMoja@users.noreply.github.com>
2022-01-21 07:14:31 +0200

    Update LEVEL 0 CODING CHALLENGES-Task0.10.js

--- LEVEL 0 CODING CHALLENGES-Task0.10.js
+++ LEVEL 0 CODING CHALLENGES-Task0.10.js
@@ -1,29 +1,29 @@
-function textCompare(text1, text2) {
-    element = new Set();
-    let myIterator = element.values()
-    for (let i = 0; i < text2.length; i++) {
+    function textCompare(text1, text2) {
+        element = new Set()
+        let myIterator = element.values()
+        for (let i = 0; i < text2.length; i++) {
 
 
-        for (let index = 0; index < text1.length; index++) {
+            for (let index = 0; index < text1.length; index++) {
 
-            if (text2[i] === text1[index]) {
-                element.add(text1[index])
+                if (text2[i] === text1[index]) {
+                    element.add(text1[index])
+                }
+
             }
 
         }
 
+        let text = ""
+        for (const entry of myIterator) {
+
+            text += entry
+            text += ","
+
+        }
+
+        return console.log("Common Letters:", text);
+
     }
 
-    let text = "";
-    for (const entry of myIterator) {
-
-        text += entry
-        text += ","
-
-    }
-
-    return console.log("Common Letters:", text);
-
-}
-
-textCompare("housesss", "computeees")
+    textCompare("housesss", "computeees")
