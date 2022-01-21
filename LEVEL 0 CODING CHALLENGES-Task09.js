92b3515d68a95bc471083ed66530d603ba83db52
Mojalefa MJAY Moja <87690422+MojalefaMoja@users.noreply.github.com>
2022-01-21 07:24:36 +0200

    Update LEVEL 0 CODING CHALLENGES-Task09.js

--- LEVEL 0 CODING CHALLENGES-Task09.js
+++ LEVEL 0 CODING CHALLENGES-Task09.js
@@ -1,51 +1,51 @@
-function findVowels(text) {
+    function findVowels(text) {
 
-    const vowels = new Set();
+        const vowels = new Set()
 
 
 
-    for (let index = 0; index < text.length; index++) {
+        for (let index = 0; index < text.length; index++) {
 
-        switch (text[index].toLowerCase()) {
-            case 'A', 'a':
-                vowels.add(text[index].toLowerCase())
-                break;
-            case 'E', 'e':
-                vowels.add(text[index].toLowerCase())
-                break;
-            case 'I', 'i':
-                vowels.add(text[index].toLowerCase())
-                break;
-            case 'O', 'o':
-                vowels.add(text[index].toLowerCase())
-                break;
-            case 'U', 'u':
-                vowels.add(text[index].toLowerCase())
-                break;
+            switch (text[index].toLowerCase()) {
+                case "A", "a":
+                    vowels.add(text[index].toLowerCase())
+                    break;
+                case "E", "e":
+                    vowels.add(text[index].toLowerCase())
+                    break;
+                case "I", "i":
+                    vowels.add(text[index].toLowerCase())
+                    break;
+                case "O", "o":
+                    vowels.add(text[index].toLowerCase())
+                    break;
+                case "U", "u":
+                    vowels.add(text[index].toLowerCase())
+                    break;
+
+            }
+
+
+
+
 
         }
 
+        const myIterator = vowels.values();
+        let letter = ''
+
+        for (const entry of myIterator) {
+
+            letter += entry + " ,"
+
+        }
+
+        console.log("Vowels :" + letter);
+        return letter
 
 
 
 
     }
 
-    const myIterator = vowels.values();
-    let letter = '';
-
-    for (const entry of myIterator) {
-
-        letter += entry + ' ,';
-
-    }
-
-    console.log('Vowels :' + letter);
-    return letter
-
-
-
-
-}
-
-findVowels('Umuzi');
+    findVowels("Umuzi")
