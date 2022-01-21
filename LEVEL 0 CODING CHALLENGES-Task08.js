6bb4138a0491711f2daaab9f82f7063d77b7043b
Mojalefa MJAY Moja <87690422+MojalefaMoja@users.noreply.github.com>
2022-01-21 07:21:31 +0200

    Update LEVEL 0 CODING CHALLENGES-Task08.js

--- LEVEL 0 CODING CHALLENGES-Task08.js
+++ LEVEL 0 CODING CHALLENGES-Task08.js
@@ -1,41 +1,41 @@
-function timeConversion(time) {
-    if (time === 60 || time < 60) {
+    function timeConversion(time) {
+        if (time === 60 || time < 60) {
 
-        hour = 0
-        mins = time
-        stampHr = ""
-        stampMin = ""
+            hour = 0
+            mins = time
+            stampHr = ""
+            stampMin = ""
 
-    } else {
-        hour = Math.round(time / 60)
-        mins = time - (60 * hour)
-        stampHr = ""
-        stampMin = ""
+        } else {
+            hour = Math.round(time / 60)
+            mins = time - (60 * hour)
+            stampHr = ""
+            stampMin = ""
+        }
+
+
+
+
+        if (hour > 1 || hour === 0) {
+
+            stampHr = " Hours"
+
+        } else if (hour === 1) {
+            stampHr = " Hour"
+        }
+
+        if (mins > 1 || mins === 0) {
+
+            stampMin = " Minutes"
+
+        } else if (mins = 1) {
+            stampMin = " Minute"
+
+        }
+
+
+        return String(hour) + stampHr + ", " + mins + stampMin;
+
     }
 
-
-
-
-    if (hour > 1) {
-
-        stampHr = " Hours"
-
-    } else {
-        stampHr = " Hour"
-    }
-
-    if (mins > 1) {
-
-        stampMin = " Minutes"
-
-    } else if (mins <= 1) {
-        stampMin = " Minute"
-
-    }
-
-
-    return String(hour) + stampHr + ", " + mins + stampMin;
-
-}
-
-console.log(timeConversion(11));
+    console.log(timeConversion(11));
